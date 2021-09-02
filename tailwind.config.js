2c3cdce8ce1657e6edef8709d43e8f874a0ab2e3
boris_montavon <boris.montavon@gmail.com>
2021-09-02 18:16:58 +0200

    [+] Webpack init, [+] interface creation, [wip] dropdown creation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,44 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      white: {
+        DEFAULT: "var(--white)",
+        hover: "var(--white-o)"
+      },
+      // For title
+      indianRed: {
+        DEFAULT: "var(--indianRed)",
+        hover: "var(--indianRed-hover)"
+      },
+      // For buttons
+      fireOpal: {
+        DEFAULT: "var(--fireOpal)",
+        hover: "var(--fireOpal-border-hover)"
+      },
+      azure: {
+        DEFAULT: "var(--azure)",
+        hover: "var(--azure-border-hover)"
+      },
+      mediumGreen: {
+        DEFAULT: "var(--mediumGreen)",
+        hover: "var(--mediumGreen-border-hover)"
+      },
+      platinum: "var(--platinum)",
+      gainsboro: "var(--gainsboro)",
+      silver: "var(--silver)"
+    },
+    extend: {
+      fontFamily: {
+        "inter": ["Inter", "sans-serif"]
+      }
+    },
+    // 'sm': '640px',
+    // 'md': '768px',
+    // 'lg': '1024px',
+    // 'xl': '1280px',
+    // '2xl': '1536px',
   },
   variants: {
     extend: {},
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/js/index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/js"),
+    filename: "[name].bundle.js",
+  },
+};
