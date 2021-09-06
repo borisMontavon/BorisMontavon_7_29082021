c004f8cb3e8c93e04b4dcb71d2b8c76bb5862b5a
boris_montavon <boris.montavon@gmail.com>
2021-09-06 19:38:23 +0200

    [+] Recipes display

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,30 @@
-import { handleDropdown } from "./filter-button";
-import { toggleDarkMode } from "./dark-mode";
+import { handleDropdown } from "./components/filter-button";
+import { recipeDisplay } from "./components/recipe-display";
+import { toggleDarkMode } from "./components/dark-mode";
 
 // Recipes' data fetch from json
-// const getData = async (url) => {
-//     const response = await fetch(url);
+const getData = async (url) => {
+    const response = await fetch(url);
 
-//     return response.json();
-// };
+    return response.json();
+};
 
-// // Home page initialization with json's data
-// const initializeHomepage = async () => {
-//     const data = await getData("assets/data.json");
-// };
+// Home page initialization with json's data
+const initializeData = async () => {
+    const data = await getData("assets/data.json");
+
+    console.log(data);
+
+    handleDropdown(data);
+
+    const recipesContainer = document.getElementById("recipes-container");
+
+    data.forEach((recipe) => {
+        recipesContainer.insertAdjacentHTML("beforeend", recipeDisplay(recipe));
+    });
+};
 
 window.addEventListener("load", () => {
-    // initializeHomepage();
-    handleDropdown();
+    initializeData();
     toggleDarkMode();
 });
