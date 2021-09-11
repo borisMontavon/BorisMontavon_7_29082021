685f0ab72eb4a24bcd5a76f3f2b2f96e921b1d0b
boris_montavon <boris.montavon@gmail.com>
2021-09-11 16:27:44 +0200

    [+] Dropdown filterable list with input value

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
-import { ingredientsDisplay, machinesDisplay, ustensilsDisplay } from "./components/dropdowns-lists-display";
+import { displayList } from "./services/display-lists-service";
+import { filterList } from "./services/filter-list";
 import { getDistinctIngredientsList } from "./services/ingredients-list";
 import { getDistinctMachineList } from "./services/machine-list";
 import { getDistinctUstensilsList } from "./services/ustensils-list";
 import { handleDropdown } from "./components/filter-button";
 import { recipeDisplay } from "./components/recipe-display";
+import Singleton from "./services/static-list-container";
 import { toggleDarkMode } from "./components/dark-mode";
 import topButton from "./components/top-button";
 
@@ -18,15 +20,18 @@ const getData = async (url) => {
 const initializeData = async () => {
     const data = await getData("assets/data.json");
 
-    // console.log(data);
-
     // Handle the way the dropdown displays itself
     handleDropdown();
 
     // Render the list of ingredients, machines and ustensils in their respectives inputs
-    ingredientsDisplay(getDistinctIngredientsList(data));
-    machinesDisplay(getDistinctMachineList(data));
-    ustensilsDisplay(getDistinctUstensilsList(data));
+    let singleton = new Singleton();
+
+    singleton.ingredientsList = getDistinctIngredientsList(data);
+    singleton.machinesList = getDistinctMachineList(data);
+    singleton.ustensilsList = getDistinctUstensilsList(data);
+
+    displayList();
+    filterList();
 
     // Render all the recipes in its container
     const recipesContainer = document.getElementById("recipes-container");
